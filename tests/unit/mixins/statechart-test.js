94f0089b0d32d574656c5ff0ec961ae75fcc5620
Michael Klein <michael@firstiwaslike.com>
2018-05-07 11:45:45 +0200

    Add statecharts mixin

---
+++ tests/unit/mixins/statechart-test.js
@@ -0,0 +1,43 @@
+import EmberObject, { get } from '@ember/object';
+import StatechartMixin from 'ember-statecharts/mixins/statechart';
+import { module, test } from 'qunit';
+
+module('Unit | Mixin | statechart', function() {
+  test('it adds statechart functionality to an ember-object', async function (assert) {
+    assert.expect(5);
+
+    let StatechartObject = EmberObject.extend(StatechartMixin);
+
+    let testData = { wat: 'lol' };
+
+    let subject = StatechartObject.create({
+      statechart: {
+        initialState: 'new',
+        states: {
+          new: {
+            exitState() {
+              assert.ok(true, 'exitState was called');
+            },
+            events: {
+              woot() {
+                assert.ok(true, 'event was called');
+                this.goToState('foo', testData);
+              }
+            }
+          },
+          foo: {
+            enterState(data) {
+              assert.deepEqual(data, testData);
+            }
+          }
+        }
+      }
+    });
+
+    assert.equal(get(subject, 'states.currentState.name'), 'new');
+
+    await get(subject, 'states').send('woot');
+
+    assert.equal(get(subject, 'states.currentState.name'), 'foo');
+  });
+});
