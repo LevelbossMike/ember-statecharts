67600db991d37271368cdfc55e62dadfce24088d
Michael Klein <michael@firstiwaslike.com>
2018-09-02 20:36:26 +0200

    Use ember-auto-import to bundle xstate

--- index.js
+++ index.js
@@ -2,14 +2,4 @@
 
 module.exports = {
   name: 'ember-statecharts',
-
-  included(app) {
-    this._super.included.apply(this, arguments);
-
-    app.import('node_modules/xstate/dist/xstate.js', {
-      using: [
-        { transformation: 'amd', as: 'xstate' }
-      ]
-    });
-  }
 };
