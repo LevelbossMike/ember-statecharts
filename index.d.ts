479b42b287bee5d5efdd2848d4623c87a6c2a95a
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-05-24 14:43:20 -0400

    chore: change ember-resource to ember-usable

--- index.d.ts
+++ index.d.ts
@@ -8,9 +8,15 @@ import {
 // NOTE: there is a lot of repetition here :(
 // Repetition will be less when ember-statecharts _is_ TS?
 
-type WithConfigParams<Context, Schema, Events extends EventObject> = Parameters<StateMachine<Context, Schema, Events>['withConfig']>;
-type WithContextParams<Context, Schema, Events extends EventObject> = Parameters< StateMachine<Context, Schema, Events>['withContext']>;
-type Send<Context, Schema, Events extends EventObject> = Interpreter<Context, Schema, Events>['send'];
+
+// Private Types
+// For building the Public Types?
+// C = Context
+// S = Schema
+// E = Events
+type WithConfigParams<C, S, E extends EventObject> = Parameters<StateMachine<C, S, E>['withConfig']>;
+type WithContextParams<C, S, E extends EventObject> = Parameters< StateMachine<C, S, E>['withContext']>;
+type Send<C, S, E extends EventObject> = Interpreter<C, S, E>['send'];
 
 type InterpretedUsable<Context, Schema, Events extends EventObject> = {
   state: State<Context, Events>;
