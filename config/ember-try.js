b87c255c6e2f9155d347c185dc4a94d92581b54f
Michael Klein <michael@firstiwaslike.com>
2020-09-09 21:12:17 +0200

    chore: upgrade ember v3.21.x

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,18 +6,18 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-3.15', // First Octane release
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.15.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
             'ember-source': '~3.16.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.5',
           },
         },
       },
@@ -54,8 +54,18 @@ module.exports = async function () {
         },
         npm: {
           devDependencies: {
-            '@ember/jquery': '^0.5.1',
+            '@ember/jquery': '^1.1.0',
           },
+        },
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false,
+          }),
         },
       },
     ],
