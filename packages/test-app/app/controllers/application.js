25a3cab60a91d69a32888457b367bf1633c22a46
Michael Klein <michael@firstiwaslike.com>
2021-11-20 09:16:12 +0100

    feat[test-app]: demonstrate use-machine resource

--- packages/test-app/app/controllers/application.js
+++ packages/test-app/app/controllers/application.js
@@ -1,6 +1,24 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { statechart } from 'ember-statecharts/computed';
+import { useMachine } from 'ember-statecharts';
+import { createMachine } from 'xstate';
+
+const fuMachine = createMachine({
+  initial: 'lol',
+  states: {
+    lol: {
+      on: {
+        FUBAR: 'fubar',
+      },
+    },
+    fubar: {
+      on: {
+        FUBAR: 'lol',
+      },
+    },
+  },
+});
 
 export default class ApplicationController extends Controller {
   @statechart({
@@ -20,8 +38,19 @@ export default class ApplicationController extends Controller {
   })
   statechart;
 
+  wat = useMachine(this, () => {
+    return {
+      machine: fuMachine,
+    };
+  });
+
   @action
   toggle() {
     this.statechart.send('TOGGLE');
+  }
+
+  @action
+  fubar() {
+    this.wat.send('FUBAR');
   }
 }
